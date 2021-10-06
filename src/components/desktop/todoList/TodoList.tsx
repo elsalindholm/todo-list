3dd01794eea8a4ffb97acebcd4384d5327842dd2
elsalindholm <31487907+elsalindholm@users.noreply.github.com>
2021-10-06 15:05:43 +0100

    Update TodoList.tsx

--- src/components/desktop/todoList/TodoList.tsx
+++ src/components/desktop/todoList/TodoList.tsx
@@ -1,11 +1,23 @@
 import { observer } from 'mobx-react';
 import React from 'react';
+import { AppState } from '../../../state/AppState';
+import { ListHeader } from './ListHeader';
 
 import './todo-list.scss';
 
+interface TodoListProps {
+  appState: AppState;
+}
+
 @observer
-export class ToDoList extends React.PureComponent {
+export class TodoList extends React.PureComponent<TodoListProps> {
   render() {
-    return <div className={'todo-list'}></div>;
+    const { appState } = this.props;
+
+    return (
+      <div className={'todo-list'}>
+        <ListHeader appState={appState} />
+      </div>
+    );
   }
 }
